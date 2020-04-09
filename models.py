1ddc252a7d571a4d421272c285e6e86c38759b93
jihamb <jimmy.hamel@polymtl.ca>
2020-04-08 22:08:41 -0400

    Added layers to DcNN

--- models.py
+++ models.py
@@ -4,15 +4,22 @@ class DcNN(nn.Module):
     def __init__(self, nLayers=17):
         super().__init__()
         self.layers = []
+        nb_features = 64
 
         # Input layer
         layers.append(nn.Cond2d()) 
+        layers.append(nn.ReLU()) #nn.ReLU(inplace=True) ??
 
         # Hidden layers
         for i in range(0, nLayers):
             layers.append(nn.Cond2d())
+            layers.append(nn.BatchNorm2d(nb_features))
+            layers.append(nn.ReLU())
 
         # Output layer
         layers.append(nn.Cond2d()) 
 
+        self.model= nn.Sequential(*layers) #Runs all layer one by one
+
     def forward(self, image):
+        return self.model(image)
