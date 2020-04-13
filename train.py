c17ad076d133d183f580c68bcb8b7910195b384a
jihamb <jimmy.hamel@polymtl.ca>
2020-04-13 15:37:46 -0400

    copied dataset.py from articles github, added code to train model

--- train.py
+++ train.py
@@ -1,19 +1,66 @@
+import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader
+from dataset import prepare_data, Dataset
+from torch.autograd import Variable
+import numpy as np
+from skimage.measure.simple_metrics import compare_psnr
+
+
+
+train_data = Dataset(train=True)
+
+valid_data = Dataset(train=False)
+
+train_loader = DataLoader(dataset=train_data, num_workers=4, batch_size=128, shuffle=True)
+
+def calculate_PSNR(noised_image, clean_image):
+    noised_image_cpu = noised_image.data.cpu().numpy().astype(np.float32)
+    clean_image_cpu = clean_image.data.cpu().numpy().astype(np.float32)
+    PSNR = 0
+    for i in range(noised_image_cpu.shape[0]):
+        # function from skimage to calculate PSNR between 2 images
+        # data range is use to know the maximum value of the pixel
+        PSNR += compare_psnr(clean_image_cpu[i,:,:,:], noised_image_cpu[i,:,:,:], data_range=1.) 
+    
+    # return mean PSNR between images
+    return (PSNR/noised_image_cpu.shape[0])
 
 def train(model, X_train, y_train, optimizer, criterion):
     model.train()
     model.zero_grad()
     optimizer.zero_grad()
     loss_training = 0.0
+    noise_level=50 # 15,25, 50 sont discuté dans le text. On pourra le rendre optionnel plus tard.
 
     # Training with SGD ?
-    for i in range(0,1): # Placeholder loop  
-        # output = model(INPUT)
-        # loss = criterion(...)
+    for batch_idx, (data, target) in enumerate(train_loader):
+        noise = torch.FloatTensor(data.size()).normal_(mean=0, std=noise_level/255.)
+        noised_image = data + noise
+        # sending values to GPU
+        noised_image, data = Variable(noised_image.cuda()), Variable(data.cuda())
+        noise = Variable(noise.cuda())
+
+        # Running Model
+        output = model(noised_image)
+
+        # TODO: Calculate loss
+        #loss = criterion(out_train, noise) / (imgn_train.size()[0]*2)
+        #loss.backward()
+        optimizer.step()
+        model.eval()
+
+        # Le github a model(imgn_train) a la place de output pour X raison. A tester
+        # Calculer l'image denoised
+        denoised_image = torch.clamp(noised_image-output, 0., 1.) 
+        psnr = calculate_PSNR(output, data)
+        
+
+        optimizer.zero_grad()
+        output = model(data)  # calls the forward function
         loss.backward()
         optimizer.step()
-        loss_training += loss.item() # not sure if we can use item for SGD
 
     loss_training /= len(X_train) # averaging the loss for this epoch
 
