74bfb28a27d75e93655e38feda4c68b98ad6c8fc
MouradLachhab <mourad.lachhab@yahoo.com>
2020-04-16 14:08:19 -0400

    Added convolutional parameters

--- models.py
+++ models.py
@@ -1,23 +1,28 @@
 import torch.nn as nn
 
-class DcNN(nn.Module):
-    def __init__(self, nLayers=17):
-        super().__init__()
-        self.layers = []
+# TODO: Remove useless comments once done with the project
+
+class DnCNN(nn.Module):
+    def __init__(self,channels, nLayers=17):
+        super(DnCNN, self).__init__()
+        layers = []
+        kernel_size = 3
+        padding = 1
         nb_features = 64
 
         # Input layer
-        layers.append(nn.Cond2d()) 
-        layers.append(nn.ReLU()) #nn.ReLU(inplace=True) ??
+        # Bias False is used as they say it has very little effect on bigger networks for convolutions
+        layers.append(nn.Cond2d(in_channels=channels, out_channels=nb_features, kernel_size=kernel_size, padding=padding, bias=False)) 
+        layers.append(nn.ReLU(inplace=True)) # I put it in cause its in the original code, but some say online that it is not always good to use. Not entirely sure why
 
         # Hidden layers
         for i in range(0, nLayers):
-            layers.append(nn.Cond2d())
+            layers.append(nn.Cond2d(in_channels=nb_features, out_channels=nb_features, kernel_size=kernel_size, padding=padding, bias=False))
             layers.append(nn.BatchNorm2d(nb_features))
-            layers.append(nn.ReLU())
+            layers.append(nn.ReLU(inplace=True))
 
         # Output layer
-        layers.append(nn.Cond2d()) 
+        layers.append(nn.Cond2d(in_channels=nb_features, out_channels=channels, kernel_size=kernel_size, padding=padding, bias=False)) 
 
         self.model= nn.Sequential(*layers) #Runs all layer one by one
 
